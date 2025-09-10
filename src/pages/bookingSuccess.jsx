8e6a50ae9de6f268bfe07efc24c78c29ca0e48aa
sinan abdulla <sinanabdulla822@gmail.com>
2025-09-10 21:27:59 +0530

    fin

--- src/pages/bookingSuccess.jsx
+++ src/pages/bookingSuccess.jsx
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 function BookingSuccess() {
   return (
     <div className="flex flex-col min-h-screen bg-white items-center justify-center">
-      <h1 className="text-4xl font-bold text-green-600 mb-6">🎉 Booking Successful!</h1>
+      <h1 className="text-4xl font-bold text-green-600 mb-6 text-center sm:text-center">
+        🎉 Booking Successful!
+      </h1>
+
       <p className="mb-6 text-black">Your logistics vehicle booking has been confirmed.</p>
       <Link to="/">
         <button className="btn btn-primary">Go to Home</button>
